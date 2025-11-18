55f98c194cce783d700b7266b5d0b828b30832c8
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2025-11-18 17:21:48 +0900

    first commit

---
+++ lib/agent_status_bulb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "agent_status_bulb/version"
+
+module AgentStatusBulb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/agent_status_bulb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AgentStatusBulb do
+  it "has a version number" do
+    expect(AgentStatusBulb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
